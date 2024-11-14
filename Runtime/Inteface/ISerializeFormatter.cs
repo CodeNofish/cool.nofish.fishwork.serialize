ba459a4d16a9075f1a09b159afaf91a2de4bfbc4
CodeNofish <1980114953@qq.com>
2024-11-14 13:34:00 +0800

    update

---
+++ Runtime/Inteface/ISerializeFormatter.cs
@@ -0,0 +1,19 @@
+﻿namespace Fishwork.Serialize {
+
+  /// <summary>
+  /// 序列化格式化器
+  /// </summary>
+  public interface ISerializeFormatter<T> {
+    /// <summary>
+    /// 序列化过程
+    /// </summary>
+    public void Serialize(T value);
+
+    /// <summary>
+    /// 反序列化过程
+    /// </summary>
+    /// <returns></returns>
+    public T Deserialize();
+  }
+
+}
---
+++ Runtime/Misc/SerializeException.cs
@@ -0,0 +1,10 @@
+﻿using Fishwork.Core;
+
+namespace Fishwork.Serialize {
+
+  /// <summary>
+  /// 序列化相关异常
+  /// </summary>
+  internal class SerializeException : FishworkException { }
+
+}
